aae431f2545d1f8b3273534bd29f024ba587545c
Joaquin Navarro <inavarrojoaquin@gmail.com>
2022-08-21 09:31:15 +0200

    Add project files.

---
+++ GartnerApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+if (args.Length == 0 || args.Length != 3)
+{
+    Console.WriteLine("No arguments or arguments different of 3");
+    ShowMessageToFinishProgram();
+    return;
+}
+
+
+
+if (args[0].ToLower() == "import")
+{
+    if (args[1].ToLower() == "capterra")
+    {
+        Console.WriteLine("Source: ", args[2]);
+    }
+
+    if (args[1].ToLower() == "softwareadvice")
+    {
+        Console.WriteLine("Source: ", args[2]);
+    }
+}
+
+ShowMessageToFinishProgram();
+
+static void ShowMessageToFinishProgram()
+{
+    Console.WriteLine("Press any key to finish...");
+    Console.ReadKey();
+}
