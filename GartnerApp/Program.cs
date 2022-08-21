d3fd6cea603f75715d3f367d3cf02219b681122f
Joaquin Navarro <inavarrojoaquin@gmail.com>
2022-08-21 14:14:15 +0200

    Added default debug args for testing

--- GartnerApp/Program.cs
+++ GartnerApp/Program.cs
@@ -4,10 +4,6 @@ try
 {
     StartProgram startProgram = new StartProgram(args);
     startProgram.Run();
-
-    // To Debug use these lines and comment the previous ones
-    //string[] testArgs = { "import", "capterra", "feed-products/capterra.yaml" };
-    //StartProgram startProgram = new StartProgram(testArgs);
 }
 catch (Exception ex)
 {
